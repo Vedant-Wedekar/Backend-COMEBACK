b6b564ccf8e6b7b30798baac57e6c56b00c3816f
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-15 12:12:24 +0530

    .

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000 
-require('dotenv').config()
 
-app.get('/',(req,res) =>{
-    res.send("Hello World");
-console.log("this is server ");
+
+
+
+// Define PORT properly
+const PORT = process.env.PORT || 3000;
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello, Backend!');
 });
 
-app.listen(process.env.PORT , () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
 
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 
