e007c82fc1671a072549655542d60f19e5afcde5
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-17 10:51:48 +0530

    timestamps learning

--- Day-3/models/user.model.js
+++ Day-3/models/user.model.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     }
-});
+},{timestamps :true}
+);
 
 export const User = mongoose.model('User', userSchema);
