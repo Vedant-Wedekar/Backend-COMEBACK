ad0747b6198bf06491a25f2f2f0ddef758333568
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-24 14:45:20 +0530

    Api Error handler

--- full-stack/src/utils/ApiError.js
+++ full-stack/src/utils/ApiError.js
@@ -5,5 +5,22 @@ class ApiError extends Error {
         error = [],
         stack=""
     )
-{}
+{
+
+    super(message);
+    this.statusCode = statusCode;
+    this.error = this.error;
+    this.data = null
+    this.message = message;
+    this.success = false;
+    if(stack){
+        this.stack = stack;
+    }else{
+        Error.captureStackTrace(this, this.constructor);
+    }
+    // this.stack = stack;
+
 }
+}
+
+export {ApiError} 
