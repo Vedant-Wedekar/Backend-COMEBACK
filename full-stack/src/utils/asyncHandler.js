4681858533366fb8a719f3fbb3078a37a29a63af
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-25 15:11:34 +0530

    new async handler

--- full-stack/src/utils/asyncHandler.js
+++ full-stack/src/utils/asyncHandler.js
@@ -1,12 +1,6 @@
-const asyncHandler = () =>async (req, res, next) =>{
-    try{
-        await fn(req, res, next)
-    }
-    catch(error){
-        res.send(err.code || 500).json({message:error.message,success:false})
-        next(error)
-    }
-}
-    export {asyncHandler}
+    const asyncHandler = (requestHandler) =>{ return(req, res, next) =>{
+    Promise.resolve(requestHandler(req, res, next)).catch(next)
+    }}
+        export {asyncHandler}
 
-        
+            
