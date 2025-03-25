48ff23cc0a52c1178dc762088e3dcfe346992ac6
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-25 15:11:00 +0530

    error solve

--- full-stack/src/index.js
+++ full-stack/src/index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import mongoose, { connect } from 'mongoose';
 import {DB_NAME} from './constants.js'
 import DBconnect from './db/index.js';
-import app from '../src/app.js'
+import { app } from '../src/app.js'
 dotenv.config({path:'./config.env'});
 DBconnect().then(() => {
     app.listen(process.env.PORT,()=>{console.log(`server is running on port ${process.env.PORT}`)})
