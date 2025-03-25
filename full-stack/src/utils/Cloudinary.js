3f3cfec158daa710729dfb8de3e235a2573935ba
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-25 16:54:06 +0530

    proper syntx

--- full-stack/src/utils/Cloudinary.js
+++ full-stack/src/utils/Cloudinary.js
@@ -1,50 +1,28 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { log } from 'console';
 import fs from 'fs';
 
-
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
-    });
+// Configuring Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const UploadonCloudinary = async (file) => {
+    try {
+        if (!file) {
+            throw new Error('No file uploaded');
+        }
+        
+        const result = await cloudinary.uploader.upload(file.path, { resource_type: 'auto' });
+        console.log("File uploaded successfully", result.url);
+        
+        return result;
+    } catch (error) {
+        fs.unlinkSync(file.path);
+        console.error(error);
+        return null;
+    }
+};
 
-
-    try
-{
-if (!file) {
-    throw new Error('No file uploaded');
-}
-const result = await cloudinary.uploader.upload(file.path, {resource_type: 'auto'});
-
-console.log("File uploaded successfully", result.url);
-return result;
-}
-
-
-catch(error){
-    fs.unlinkSync(file.path);
-    console.log(error);
-    return null;
-}
-
-//     // Upload a file
-//     // const uploadResult = await cloudinary.uploader
-//     // Upload an image
-//     const uploadResult = await cloudinary.uploader
-//     .upload(
-//         'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
-//             public_id: 'shoes',
-//         }
-//     )
-//     .catch((error) => {
-//         console.log(error);
-//     });
- 
-//  console.log(uploadResult);}
-}
- export {UploadonCloudinary}
-// export {UploadonCloudinary}
+export { UploadonCloudinary };
