6231cd3414a1682f0b8cca8578668132e4397cb9
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-25 15:12:27 +0530

    user controller and route added

---
+++ full-stack/src/routes/user.routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+import { registerUser } from "../controllers/user.controller.js";
+
+const router = Router();
+
+
+
+router.route('/register').post( registerUser);
+
+
+export { router as UserRouter };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
