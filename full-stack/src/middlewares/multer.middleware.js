44ddcea702eee992ecf2a9716bb77ac021d0707d
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-03-30 09:49:05 +0530

    prettier code

--- full-stack/src/middlewares/multer.middleware.js
+++ full-stack/src/middlewares/multer.middleware.js
@@ -1,20 +1,22 @@
 import multer from 'multer';
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
+    destination: (req, file, cb) => {
+        cb(null, './public/temp');
     },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-
-      cb(null, file.originalname)
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
     }
-  })
-  
-  const upload = multer({ storage: storage })
-  export {  upload }
+});
+
+const upload = multer({ storage });
+export { upload };
 
 
 
 
-  
+
+
+
+
+
--- full-stack/src/models/video.model.js
+++ full-stack/src/models/video.model.js
@@ -1,37 +1,16 @@
-import mongoose , {Schema} from 'mongoose';
-import mongooseAggregatePaginate from
-  'mongoose-aggregate-paginate-v2';
+import mongoose, { Schema } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
-const videosSchema = new mongoose.Schema({
+const videoSchema = new Schema({
+    videoFile: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    owner: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number },
+    view: { type: Number, default: 0 },
+    isPublished: { type: Boolean, default: true }
+}, { timestamps: true });
 
-
-videoFile:{
-  type:String,
-  required:true
-},thumbnail:{
-  type:String,
-  required:true
-},owner:[{
-  type:mongoose.Schema.Type.ObjectId,
-  ref:'User'
-}],title:{
-  type:String,
-  required:true
-},description:{
-  type:String,
-  required:true
-},duration:{
-  type:Number,
- 
-},view:{
-  type:Number,
-  required:true ,
-  default:0
-},isPublished:{
-  type:boolean,
-  required:true
-  ,default:true
-},
-}, {timestamps:true});
 videoSchema.plugin(mongooseAggregatePaginate);
-export const Video = mongoose.model("Video",videosSchema);
+export const Video = mongoose.model("Video", videoSchema);
