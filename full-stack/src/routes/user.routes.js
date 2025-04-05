74c7d282ead72879cefb0a3b3311d19506a70c18
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-05 09:19:16 +0530

    upload field

--- full-stack/src/routes/user.routes.js
+++ full-stack/src/routes/user.routes.js
@@ -1,13 +1,33 @@
 import { Router } from "express";
 
 import { registerUser } from "../controllers/user.controller.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
+import { loginUser , logoutUser , generateAccessTokenAndRefreshToken } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 
 
-router.route('/register').post( registerUser);
+router.route('/register').post(
 
+    upload.fields([
+        {
+            name: 'avatar',
+            maxCount: 1
+        },
+        {
+            name: 'coverImage',
+            maxCount: 1
+        }
+    ]),
+    
+    
+    registerUser);
+
+
+    router.route('/login').post(loginUser)
+
+    router.route('/logout').post(verifyJWT,logoutUser)
 
 export { router as UserRouter };
 
