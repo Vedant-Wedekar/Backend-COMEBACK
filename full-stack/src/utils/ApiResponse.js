1ac86dffe13287dc2412fd68535bdffec406ca56
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-05 20:29:37 +0530

    Refactor ApiResponse class for improved message consistency and formatting

--- full-stack/src/utils/ApiResponse.js
+++ full-stack/src/utils/ApiResponse.js
@@ -1,10 +1,10 @@
-class ApiResponse  {
-    constructor(statusCode, message="success", data) {
-
+class ApiResponse {
+    constructor(statusCode, message = "Success", data) {
         this.message = message;
         this.success = statusCode >= 200 && statusCode < 300;
         this.statusCode = statusCode;
         this.data = data;
-    
     }
 }
+
+export { ApiResponse };
