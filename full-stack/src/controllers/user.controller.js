2a3cadc70be709e58a4545521649c9a20bac39bf
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-06 11:44:22 +0530

    Add getUserChannelProfile function to retrieve user channel details and subscription status

--- full-stack/src/controllers/user.controller.js
+++ full-stack/src/controllers/user.controller.js
@@ -76,7 +76,6 @@ const registerUser = asyncHandler( async (req, res) => {
    return res.status(201).json(new ApiResponse(201, "User registered successfully", createdUser));
 });
 
-
 const loginUser = asyncHandler(async (req, res) => {
      
     const { email, password ,username} = req.body;
@@ -117,7 +116,6 @@ const loginUser = asyncHandler(async (req, res) => {
         json(new ApiResponse(200, "User logged in successfully", loggedInUser));
 });
 
-
 const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
@@ -142,7 +140,6 @@ const logoutUser = asyncHandler(async(req, res) => {
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User logged Out"))
 })
-
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
@@ -192,7 +189,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
-
 const changeCurrentPassword = asyncHandler(async(req, res) => {
     const {oldPassword, newPassword} = req.body
 
@@ -212,7 +208,6 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
     .status(200)
     .json(new ApiResponse(200, {}, "Password changed successfully"))
 })
-
 
 const getCurrentUser = asyncHandler(async(req, res) => {
     return res
@@ -314,8 +309,83 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
         new ApiResponse(200, user, "Cover image updated successfully")
     )
 })
+
+const getUserChannelProfile = asyncHandler(async(req, res) => {
+    const {username} = req.params
+
+    if (!username?.trim()) {
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+            }
+        }
+    ])
+
+    if (!channel?.length) {
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfully")
+    )
+})
+
+
+
 export {
-    
+    getUserChannelProfile,
      registerUser,
      loginUser, 
      logoutUser, 
