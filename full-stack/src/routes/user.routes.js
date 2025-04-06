dfec4bd8d81f5f5d91db70e7c43542e8fccbeed9
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-04-06 11:44:34 +0530

    Add refresh token route to user authentication

--- full-stack/src/routes/user.routes.js
+++ full-stack/src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser , logoutUser , generateAccessTokenAndRefreshToken } from "../controllers/user.controller.js";
+import { loginUser , logoutUser , generateAccessTokenAndRefreshToken , refreshAccessToken } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
@@ -28,6 +28,8 @@ router.route('/register').post(
     router.route('/login').post(loginUser)
 
     router.route('/logout').post(verifyJWT,logoutUser)
+
+    router.route('/refresh-token').post(refreshAccessToken)
 
 export { router as UserRouter };
 
